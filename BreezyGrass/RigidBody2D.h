b093a550e5a43fa28d3ddeba29e83faaf6fc1c1d
Jamie Kabzinski <jamie_r_kabzinski@hotmail.com>
2023-04-04 08:50:54 +0930

    changed moment to float

--- BreezyGrass/RigidBody2D.h
+++ BreezyGrass/RigidBody2D.h
@@ -12,7 +12,7 @@ public:
 	Vector2<float> velocity{ 0.f, 0.f };
 	Vector2<float> angular_velocity{ 0.f, 0.f };
 	Vector2<float> spring_force{ 0.f, 0.f };
-	Vector2<float> spring_moment{ 0.f, 0.f };
+	float spring_moment{ 0.f };
 	float angle{ 0.f }; // radians
 	float mass_kg{ 0.5f };
 	float speed{ 0.f };
