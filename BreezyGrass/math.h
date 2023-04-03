73375c2fde41d72947ad7148844cb1742260da2d
Jamie Kabzinski <jamie_r_kabzinski@hotmail.com>
2023-04-04 08:50:18 +0930

    fixed Vector rotate function

--- BreezyGrass/math.h
+++ BreezyGrass/math.h
@@ -8,10 +8,10 @@ Vector2<T> rotate_2D(const double angle_radians, const Vector2<T>& point)
 	const double sina = sin(angle_radians);
 	const double cosa = cos(angle_radians);
 
-	const Vector2<double> rotated_point{
-		point.x() * cosa - point.y() * sina,
-		point.x() * sina + point.y() * cosa
+	const Vector2<T> rotated_point {
+		static_cast<T>( point.x() * cosa - point.y() * sina ),
+		static_cast<T>( point.x() * sina + point.y() * cosa )
 	};
 
-	return static_cast<Vector2<T>(rotated_point);
+	return rotated_point;
 }
